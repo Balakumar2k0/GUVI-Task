8a073e8c5b506f411d368fdf8706e5851ed9c203
Balakumar2k0 <107246991+Balakumar2k0@users.noreply.github.com>
2023-11-28 10:16:57 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
-// Establish a connection to your MySQL database
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,18 +6,13 @@ $dbname = "signup";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve form data
 $email = $_POST['Email'];
 $password = $_POST['Password'];
 
-// Add more secure password handling (e.g., hashing) in a production environment
-
-// Check if the user exists
 $sql = "SELECT * FROM register WHERE Email='$email' AND Password='$password'";
 $result = $conn->query($sql);
 
